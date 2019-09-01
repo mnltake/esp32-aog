c6640cb5b69ce73b693bfde6794e9fe66f2f4019
Johannes Formann <johannes.formann@uni-duisburg-essen.de>
2019-09-01 03:49:59 +0200

    catch erneous port access

--- src/io_access.cpp
+++ src/io_access.cpp
@@ -78,31 +78,31 @@
 
   // configure a port, default nothing enabled
   bool IoAccess::configureAsDigitalInput(uint8_t port, bool usePullUpDown, bool pullDirectionUp) {
-    if (port == 255) {
+    if (port == 255 || provider[port / 32]  == nullptr) {
       return false;
     }
     return provider[port / 32] ->  configureAsDigitalInput(port % 32, usePullUpDown, pullDirectionUp);
   }
   bool IoAccess::configureAsAnalogInput(uint8_t port) {
-    if (port == 255) {
+    if (port == 255 || provider[port / 32]  == nullptr) {
       return false;
     }
     return provider[port / 32] ->  configureAsAnalogInput(port % 32);
   }
   bool IoAccess::configureAsDigitalOutput(uint8_t port) {
-    if (port == 255) {
+    if (port == 255 || provider[port / 32]  == nullptr) {
       return false;
     }
     return provider[port / 32] ->  configureAsDigitalOutput(port % 32);
   }
   bool IoAccess::configureAsPwmOutput(uint8_t port, uint16_t frequency) {
-    if (port == 255) {
+    if (port == 255 || provider[port / 32]  == nullptr) {
       return false;
     }
     return provider[port / 32] ->  configureAsPwmOutput(port % 32, frequency);
   }
   bool IoAccess::configureAsRawIo(uint8_t port) {
-    if (port == 255) {
+    if (port == 255 || provider[port / 32]  == nullptr) {
       return false;
     }
     return provider[port / 32] ->  configureAsRawIo(port % 32);
@@ -111,14 +111,42 @@
   // returns the pin number of a port on the microcontroler.
   // should only be used if necessary (e.g. setting up serial ports)
   // other application has to configure the pin
-  gpio_num_t IoAccess::getRawIo(uint8_t port) {return provider[port / 32] -> getRawIo(port % 32);}
+  gpio_num_t IoAccess::getRawIo(uint8_t port) {
+    if (provider[port / 32]  != nullptr) {
+      return provider[port / 32] -> getRawIo(port % 32);
+    }
+    return GPIO_NUM_0;
+  }
 
   // normal interactions during runtime
-  bool IoAccess::getDigitalInput(uint8_t port) {return provider[port / 32] ->  getDigitalInput(port % 32);}
-  int IoAccess::getAnalogInput(uint8_t port) {return provider[port / 32] ->  getAnalogInput(port % 32);}
-  float IoAccess::getAnalogInputScaled(uint8_t port) {return provider[port / 32] ->  getAnalogInputScaled(port % 32);} // FSR scaled to a 0-1
-  void IoAccess::setDigitalOutput(uint8_t port, bool state) {provider[port / 32] -> setDigitalOutput(port % 32, state);}
-  void IoAccess::setPwmOutput(uint8_t port, uint8_t dutyCycle) {provider[port / 32] -> setPwmOutput(port % 32, dutyCycle);}
+  bool IoAccess::getDigitalInput(uint8_t port) {
+    if (provider[port / 32]  != nullptr) {
+      return provider[port / 32] -> getDigitalInput(port % 32);
+    }
+    return false;
+  }
+  int IoAccess::getAnalogInput(uint8_t port) {
+    if (provider[port / 32]  != nullptr) {
+      return provider[port / 32] -> getAnalogInput(port % 32);
+    }
+    return -1;
+  }
+  float IoAccess::getAnalogInputScaled(uint8_t port) {
+    if (provider[port / 32]  != nullptr) {
+      return provider[port / 32] -> getAnalogInputScaled(port % 32);
+    }
+    return -1;
+  } // FSR scaled to a 0-1
+  void IoAccess::setDigitalOutput(uint8_t port, bool state) {
+    if (provider[port / 32]  != nullptr) {
+      provider[port / 32] -> setDigitalOutput(port % 32, state);
+    }
+  }
+  void IoAccess::setPwmOutput(uint8_t port, uint8_t dutyCycle) {
+    if (provider[port / 32]  != nullptr) {
+      provider[port / 32] -> setPwmOutput(port % 32, dutyCycle);
+    }
+  }
 
 
   const String IoAccess::getPortName(uint8_t port) {
